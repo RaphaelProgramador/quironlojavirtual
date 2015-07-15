93dc47036be21e69a72fff10af3da55d67f4f08b
RaphaelProgramador <rafael.rodrigues@outlook.com>
2015-07-14 23:07:20 -0300

    Classes de Paginação

---
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -0,0 +1,25 @@
+﻿using Quiron.LojaVirtual.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class VitrineController : Controller
+    {
+        private ProdutosRepositorio repositorio;
+        private int produtosPorPagina = 3;
+        
+        // GET: Vitrine
+        public ActionResult ListarProdutos(int pagina = 1)
+        {
+            repositorio = new ProdutosRepositorio();
+
+            var produtos = repositorio.Produtos.Take(10).OrderBy(p => p.Nome).Skip((pagina - 1) * produtosPorPagina).Take(produtosPorPagina);
+
+            return View(produtos);
+        }
+    }
+}
