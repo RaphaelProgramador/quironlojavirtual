50f5b2eb4af82c48224596c41f08448ca7742a08
RaphaelProgramador <rafael.rodrigues@outlook.com>
2015-07-23 20:11:06 -0300

    Classes Adicionadas: Carrinho e ItemCarrinho

---
+++ Quiron.LojaVirtual.Dominio/Entidades/ItemCarrinho.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    public class ItemCarrinho
+    {
+        public Produto Produto { get; set; }
+        public int Quantidade { get; set; }
+
+    }
+}
